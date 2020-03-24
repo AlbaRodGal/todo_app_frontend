b72b5e3bff8398e3890cf99b947d69f483a610a7
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-03-24 12:08:08 +0000

    setting new cols: sm, md and lg to make it cross devices

--- src/AddNewTask/AddNewTask.js
+++ src/AddNewTask/AddNewTask.js
@@ -1,47 +1,46 @@
 import React from "react";
 import "./AddNewTask.css";
 
-
 function AddNewTask() {
     return <div className="row m-1">
-        <div className="col-12 col-md-4 newTask">
-            <input type="text" className="form-control" />
-        </div>
-        <div className="col-6 col-md-3" newTask>
-            <input type="date" className="form-control" />
-        </div>
-        <div className="col-2 col-md-2">
-            <div class="btn-group">
-                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Category
+            <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+                <input type="text" className="form-control" />
+            </div>
+            <div className="col-12 col-sm-5 col-md-3 col-lg-2" newTask>
+                <input type="date" className="form-control" />
+            </div>
+            <div className="col-4 col-sm-3 col-md-2 col-lg-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Category
             </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Separated link</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Home</a>
+                        <a class="dropdown-item" href="#">Learning</a>
+                        <a class="dropdown-item" href="#">Work</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="col-2 col-md-2">
-            <div class="btn-group">
-                <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Priority
+            <div className="col-4 col-sm-2 col-md-2 col-lg-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Priority
                 </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Separated link</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Urgent</a>
+                        <a class="dropdown-item" href="#">Medium</a>
+                        <a class="dropdown-item" href="#">Low</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
                 </div>
             </div>
+            <div className="col-4 col-sm-2 col-md-1 col-lg-2">
+                <button class="btn btn-secondary add">Add</button>
+            </div>
         </div>
-        <div className="col-2 col-md-1">
-            <button class="btn btn-secondary add">Add</button>
-        </div>
-    </div>
 }
 
 export default AddNewTask;
