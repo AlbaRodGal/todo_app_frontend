298f969aae9e5c7d0ce95c99daf91000f6a3aeec
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-04-09 18:22:33 +0100

    Updates

---
+++ precache-manifest.e9260d53d1a6c336842c024ac86a7b26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6bb7f2ca6812aba7ff7d480d8f08a1",
+    "url": "/todo_app_frontend/index.html"
+  },
+  {
+    "revision": "b55701ddf57f64ab6837",
+    "url": "/todo_app_frontend/static/css/2.0ac5b054.chunk.css"
+  },
+  {
+    "revision": "0edb2d18e1eff1a67491",
+    "url": "/todo_app_frontend/static/css/main.52da0202.chunk.css"
+  },
+  {
+    "revision": "b55701ddf57f64ab6837",
+    "url": "/todo_app_frontend/static/js/2.d82d2603.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo_app_frontend/static/js/2.d82d2603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edb2d18e1eff1a67491",
+    "url": "/todo_app_frontend/static/js/main.9a144df9.chunk.js"
+  },
+  {
+    "revision": "bade65ba771c6eed29fc",
+    "url": "/todo_app_frontend/static/js/runtime-main.f220f31d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo_app_frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo_app_frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo_app_frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo_app_frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo_app_frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a4c1df651dba7b2ebbce6235c09111a4",
+    "url": "/todo_app_frontend/static/media/wesley-pacifico-mPz34HvJdz8-unsplash.a4c1df65.jpg"
+  }
+]);
