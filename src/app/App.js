baf8bfd6e496eeb9b77fd633818a68fe11ab63bc
Alba Rodriguez <albarodriguezgalvez@gmail.com>
2020-04-24 09:23:46 +0100

    Added Google font

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState } from "react";
 import './App.css';
 import Header from "../Header/Header";
 import TaskCount from "../TaskCount/TaskCount";
